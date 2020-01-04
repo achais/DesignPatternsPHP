cf64098748365b3050a4f61ee227fb72012ad417
achais <achais.zheng@gmail.com>
2020-01-04 20:48:20 +0800

    数据映射模式

---
+++ Structural/DataMapper/Tests/DataMapperTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace DesignPatterns\Structural\DataMapper\Tests;
+
+use DesignPatterns\Structural\DataMapper\StorageAdapter;
+use DesignPatterns\Structural\DataMapper\User;
+use DesignPatterns\Structural\DataMapper\UserMapper;
+use PHPUnit\Framework\TestCase;
+
+class DataMapperTest extends TestCase
+{
+    public function testCanMapUserFromStorage()
+    {
+        $storage = new StorageAdapter([1 => ['username' => 'domnikl', 'email' => 'liebler.dominik@gmail.com']]);
+        $mapper = new UserMapper($storage);
+
+        $user = $mapper->findById(1);
+
+        $this->assertInstanceOf(User::class, $user);
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testWillNotMapInvalidData()
+    {
+        $storage = new StorageAdapter([]);
+        $mapper = new UserMapper($storage);
+
+        $mapper->findById(1);
+    }
+}
---
+++ Structural/DataMapper/User.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace DesignPatterns\Structural\DataMapper;
+
+class User
+{
+    /**
+     * @var string
+     */
+    private $username;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    public static function fromState(array $state): User
+    {
+        // 在你访问的时候验证状态
+
+        return new self(
+            $state['username'],
+            $state['email']
+        );
+    }
+
+    public function __construct(string $username, string $email)
+    {
+        // 先验证参数在设置他们
+
+        $this->username = $username;
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+}
